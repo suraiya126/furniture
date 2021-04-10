606db84562a15e7fe07b2db68e4e7229ec594a26
suraiya126 <82272679+suraiya126@users.noreply.github.com>
2021-04-10 13:30:28 -0400

    Add files via upload

--- Furniture/index.js
+++ Furniture/index.js
@@ -29,7 +29,7 @@ const colorBedModal = {
         double4t6: [
             'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618033024/Local/shadow_wrk_4ft6_lefv8a.webp',
             'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618053283/Local/cornell_lined_6ft_14i39_hawkqm.webp',
-            'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618038133/Local/bedbase_6ft_6i_39_jc6tky.png'
+            'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618041471/Local/bedbase_6ft_14i_39_pf4dvy.webp'
         ]
     }
 
@@ -67,25 +67,65 @@ const depth = {
 const boxImageModal = {
     blue: {
         double4t6: {
-            endDoor: 'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618045262/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i34_ycsiig.png',
-            same2Side: [
-                'https://www.youtube.com/watch?v=UuLiaIdDkqU',
-                'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618054725/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i34_c9ozc3.webp'
-            ]
+            depth12: {
+                endDoor: 'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618045262/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i34_ycsiig.png',
+                same2Side: [
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618063938/Local/de_egyyub.webp',
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618064075/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i34_mc2szx.webp'
+                ],
+                drawerSide2End: [
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618064075/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i34_mc2szx.webp',
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618045262/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i34_ycsiig.png',
+                ]
+            },
+            depth10: {
+                endDoor: 'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618045262/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i34_ycsiig.png',
+                same2Side: [
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618063938/Local/de_egyyub.webp',
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618064075/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i34_mc2szx.webp'
+                ],
+                drawerSide2End: [
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618064075/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i34_mc2szx.webp',
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618045262/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i34_ycsiig.png',
+                ]
+            }
+
         }
     },
     mustard: {
         double4t6: {
-            endDoor: 'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618047420/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i39_kr9htt.png',
-            same2Side: [
-                'https://www.youtube.com/watch?v=UuLiaIdDkqU',
-                'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618054725/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i34_c9ozc3.webp'
-            ]
+            depth10: {
+                endDoor: 'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618047420/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i39_kr9htt.png',
+                same2Side: [
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618054725/Local/reference_drawer_normal_front_200_6ft_drawer_normal_front_14i39_iqkbf7.webp',
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618054725/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i39_n26aad.webp'
+                ],
+                drawerSide2End: [
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618054725/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i39_n26aad.webp',
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618047420/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i39_kr9htt.png',
+                ]
+            },
+            depth12: {
+                endDoor: 'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618047420/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i39_kr9htt.png',
+                same2Side: [
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618054725/Local/reference_drawer_normal_front_200_6ft_drawer_normal_front_14i39_iqkbf7.webp',
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618054725/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i39_n26aad.webp'
+                ],
+                drawerSide2End: [
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618054725/Local/reference_drawer_normal_back_200_6ft_drawer_normal_front_14i39_n26aad.webp',
+                    'https://res.cloudinary.com/dltd4gs4a/image/upload/v1618047420/Local/reference_drawer_jumbo_200_drawer_jumbo_front_14i39_kr9htt.png',
+                ]
+            }
+
         }
     }
 
 }
 
+/* const headerBox = {
+    blue
+}
+ */
 
 /* Start Size selector */
 const small6Feet = document.getElementById('size-button-small-6feet')
@@ -108,8 +148,11 @@ const depth12 = document.getElementById('depth-12')
 /* Start Door selector */
 const door = document.getElementById('door')
 const endDoor = document.getElementById('endDoor')
+const side = document.querySelector('.side')
 const sameToSide1 = document.getElementById('side-1')
 const sameToSide2 = document.getElementById('side-2')
+const sameTwoButton = document.getElementById('sameToSide')
+const drawerSide2End = document.getElementById('drawerSide2End')
 /* End Door selector */
 
 
@@ -121,33 +164,86 @@ const body = document.getElementById('body')
 
 const getClickedSize = (e) => {
     const att = e.path[0].dataset.size;
-
     currentSelectedSize = att
+    console.log(currentSelectedColor, currentSelectedSize)
     if (currentSelectedSize === 'small2Ft6') {
+
         door.src = ''
         door.style.visibility = 'hidden'
+        sameToSide1.src = ''
+        sameToSide1.style.visibility = 'hidden'
+        sameToSide2.src = ''
+        sameToSide2.style.visibility = 'hidden'
     }
+    console.log(currentSelectedColor, currentSelectedSize)
     const getImage = colorBedModal[currentSelectedColor][currentSelectedSize]
     back.src = getImage[0]
     header.src = getImage[1]
     body.src = getImage[2]
+    const getDepth = depth[currentSelectedColor][currentSelectedSize][currentSelectDepth]
+    body.src = getDepth
+
+
+
+
 }
+
+/* somehow Have to modified the middle of the bed */
 
 const getCLickedColor = (e) => {
     const att = e.path[0].dataset.color;
     currentSelectedColor = att
-    const getImage = colorBedModal[att][currentSelectedSize]
-    if (currentSelectedDoor) {
-        const getDoor = boxImageModal[currentSelectedColor][currentSelectedSize][currentSelectedDoor]
+    const getImageAA = colorBedModal[att][currentSelectedSize]
+
+
+    if (currentSelectedDoor === 'endDoor' && currentSelectDepth !== 'depth6') {
+        console.log(currentSelectDepth, currentSelectedDoor, currentSelectedColor, currentSelectedSize,)
+        const getDoor = boxImageModal[currentSelectedColor][currentSelectedSize][currentSelectDepth][currentSelectedDoor]
         door.src = getDoor
         door.style.visibility = 'visible'
     }
+
+    if (currentSelectDepth === 'depth6') {
+        header.classList.add('mr-top')
+    } else {
+        header.classList.remove('mr-top')
+    }
+
+    if (currentSelectedDoor === 'same2Side') {
+        back.src = getImageAA[0]
+        header.src = getImageAA[1]
+        body.src = getImageAA[2]
+        const getImage = boxImageModal[att][currentSelectedSize][currentSelectDepth][currentSelectedDoor]
+        console.log(currentSelectedColor, currentSelectedSize, currentSelectDepth, currentSelectedDoor)
+        sameToSide1.src = getImage[0]
+        sameToSide2.src = getImage[1]
+        side.style.visibility = 'visible'
+        sameToSide2.style.visibility = 'visible'
+
+        return;
+
+    }
+
+    if (currentSelectedDoor === 'drawerSide2End') {
+        back.src = getImageAA[0]
+        header.src = getImageAA[1]
+        body.src = getImageAA[2]
+        const getImage = boxImageModal[att][currentSelectedSize][currentSelectDepth][currentSelectedDoor]
+        door.src = getImage[1]
+        door.style.visibility = 'visible'
+        sameToSide1.src = getImage[0]
+        sameToSide1.style.visibility = 'visible'
+        return
+
+    }
     if (currentSelectDepth) {
         const getHeader = depth[att][currentSelectedSize][currentSelectDepth]
-        back.src = getImage[0]
-        header.src = getImage[1]
+        back.src = getImageAA[0]
+        header.src = getImageAA[1]
         body.src = getHeader
+        return
     }
+
 
 }
 
@@ -156,21 +252,66 @@ const changedDepth = (e) => {
     currentSelectDepth = att
     const getImage = depth[currentSelectedColor][currentSelectedSize][att]
     currentSelectedImage = getImage
+
+    if (currentSelectDepth === 'depth6') {
+        sameToSide1.src = ''
+        sameToSide2.src = ''
+        door.src = ''
+    }
+
+    if (currentSelectDepth === 'depth6') {
+        header.classList.add('mr-top')
+    } else {
+        header.classList.remove('mr-top')
+    }
     body.src = getImage
 }
 
 const changedDoor = (e) => {
 /*     console.log(e.target.dataset.door)
  */    const att = e.path[0].dataset.door;
+
     if (currentSelectedSize === 'small2Ft6') {
+
         door.src = ''
         door.style.visibility = 'hidden'
         return
     }
-    currentSelectedDoor = att
-    const getImage = boxImageModal[currentSelectedColor][currentSelectedSize][att]
-    door.src = getImage
-    door.style.visibility = 'visible'
+
+    if (e.target.dataset.door === 'same2Side' && currentSelectedSize !== 'small2Ft6') {
+        currentSelectedDoor = att
+
+        door.src = ''
+        door.style.visibility = 'hidden'
+        const getImage = boxImageModal[currentSelectedColor][currentSelectedSize][currentSelectDepth][att]
+        sameToSide1.src = getImage[0]
+        sameToSide2.src = getImage[1]
+        side.style.visibility = 'visible'
+        sameToSide2.style.visibility = 'visible'
+        return;
+
+    }
+    if (e.target.dataset.door === 'endDoor' && currentSelectedSize !== 'small2Ft6') {
+        sameToSide1.src = ''
+        sameToSide2.src = ''
+        currentSelectedDoor = att
+        const getDoor = boxImageModal[currentSelectedColor][currentSelectedSize][currentSelectDepth][currentSelectedDoor]
+        door.src = getDoor
+        door.style.visibility = 'visible'
+        return
+    }
+
+    if (e.target.dataset.door === 'drawerSide2End') {
+        currentSelectedDoor = att
+        const getImage = boxImageModal[currentSelectedColor][currentSelectedSize][currentSelectDepth][e.target.dataset.door]
+        door.src = getImage[1]
+        door.style.visibility = 'visible'
+        sameToSide1.src = getImage[0]
+        sameToSide1.style.visibility = 'visible'
+
+        console.log(getImage, currentSelectedColor, currentSelectedSize, currentSelectDepth, e.target.dataset.door)
+    }
+
 
 
 }
@@ -206,6 +347,6 @@ depth12.addEventListener('click', changedDepth)
 
 /* Start Door */
 endDoor.addEventListener('click', changedDoor)
-sameToSide1.addEventListener('click', changedDoor)
-sameToSide2.addEventListener('click', changedDoor)
+sameTwoButton.addEventListener('click', changedDoor)
+drawerSide2End.addEventListener('click', changedDoor)
 /* End Door */
